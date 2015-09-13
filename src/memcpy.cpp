9d3f3ab2ee42985d4b3d45fd8fed42a98c2f3a83
ljfa-ag <ljfa-ag@live.de>
2015-09-13 13:02:31 +0200

    Create header for memcpy and fix memmove implementation

--- src/memcpy.cpp
+++ src/memcpy.cpp
@@ -1,4 +1,5 @@
-#include <cstring>
+//#include <cstring>
+#include "memcpy.h"
 
 #undef memmove
 #undef memcpy
@@ -15,7 +16,8 @@ void* memmove(void* dest, const void* src, size_t n)
     if(dest < src)
         return memcpy(dest, src, n);
 
-    for(size_t i = n-1; i >= 0; --i)
+    size_t end = static_cast<size_t>(-1);
+    for(size_t i = n-1; i != end; --i)
         static_cast<char*>(dest)[i] = static_cast<const char*>(src)[i];
     
     return dest;
